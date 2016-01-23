b16e672c0729e97d924600c31430944df967a53d
Lukeghenco <lukeghenco@gmail.com>
2016-01-23 01:14:57 -0500

    all tests are passing but I am getting a NoMethodError when I submit the form in /posts/new.html.erb

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -30,8 +30,8 @@ class PostsController < ApplicationController
 
     respond_to do |format|
       if @post.save
-        format.html { redirect_to @post, notice: 'Post was successfully created.' }
-        format.json { render action: 'show', status: :created, location: @post }
+        format.html { redirect_to post_path(@post), notice: 'Post was successfully created.' }
+        format.json { render action: 'show', status: :created, location: post_path(@post) }
       else
         format.html { render action: 'new' }
         format.json { render json: @post.errors, status: :unprocessable_entity }
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ RailsBlog::Application.routes.draw do
 
   resources :post_tags
 
+  resources :posts
+
   resources :tags
 
   resources :users
