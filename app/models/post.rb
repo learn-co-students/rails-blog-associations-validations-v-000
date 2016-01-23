9b3a651ebe34c7e61299722e97f7457699f269c8
jakebrady5 <jakebrady5@gmail.com>
2016-01-23 12:27:23 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,17 @@
 class Post < ActiveRecord::Base
-  
+  belongs_to :user
+  has_many :post_tags
+  has_many :tags, through: :post_tags  
+
+  validates :name, presence: true
+  validates :content, presence: true
+
+  accepts_nested_attributes_for :tags
+
+  def tags_attributes=(tags)
+    binding.pry
+    tags[:tag].each do |tag|
+      Tag.find_or_create(tag)
+    end
+  end
 end
