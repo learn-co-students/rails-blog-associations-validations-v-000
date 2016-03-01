d9509db529db634b20134acc89f165198c490c37
Adam King <adam.king0126@gmail.com>
2016-03-01 11:31:23 -0600

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,4 +4,8 @@ class Post < ActiveRecord::Base
   belongs_to :user
   has_many :post_tags
   has_many :tags, through: :post_tags
+
+  def tags_attributes=(attributes)
+    self.tags.build(attributes["0"])
+  end
 end
