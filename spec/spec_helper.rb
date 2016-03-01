a83ac14727986edfdfd28665a0571405aee4418c
Chris Aporta <christopher.aporta@gmail.com>
2016-02-29 20:56:19 -0500

    complete lab

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -17,6 +17,7 @@ RSpec.configure do |config|
   config.use_transactional_fixtures = false
   config.infer_base_class_for_anonymous_controllers = false
   config.order = "default"
+  config.color = true
   config.include Capybara::DSL
 
   DatabaseCleaner.strategy = :truncation
