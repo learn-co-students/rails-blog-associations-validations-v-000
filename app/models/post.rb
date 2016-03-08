340edcdca3092e796453d1a5fa4045cfabbbb7d9
pajamaw <pjpeterww@gmail.com>
2016-03-08 12:31:49 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,17 @@
 class Post < ActiveRecord::Base
-  
+  belongs_to :user
+  has_many :comments
+  has_many :post_tags
+  has_many :tags, :through => :post_tags
+
+  validates_presence_of :name, :content
+
+  accepts_nested_attributes_for :tags, reject_if: :all_blank
+
+   def tags_attributes=(tag_attributes)
+    tag_attributes.values.each do |tag_attribute|
+        tag = Tag.find_or_create_by(tag_attribute)
+        self.post_tags.build(tag: tag)
+    end
+  end
 end
