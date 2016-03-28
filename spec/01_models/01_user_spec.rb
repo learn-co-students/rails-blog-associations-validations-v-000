b48bdd22cb474daf7df9eb9f78a3c00a60fba5f9
drywaterr24 <angelolea24@gmail.com>
2016-03-27 20:27:29 -0400

    Done.

--- spec/01_models/01_user_spec.rb
+++ spec/01_models/01_user_spec.rb
@@ -1,7 +1,7 @@
 describe User do
   context 'User validations' do
     it "user is valid" do
-      expect(User.create(name: "Scbbers")).to be_valid
+      expect(User.create(name: "Scabbers")).to be_valid
     end
 
     it "invalid with no name" do
@@ -15,7 +15,7 @@ describe User do
   end
 
   context 'User relationship to Post' do
-    let(:user) { User.create(name: 'rookshanks') }
+    let(:user) { User.create(name: 'Crookshanks') }
     let!(:post1) { Post.create(user_id: user.id, name: 'post1', content: 'content1') }
     let!(:post2) { Post.create(user_id: user.id, name: 'post2', content: 'contente2') }
 
--- spec/01_models/02_post_spec.rb
+++ spec/01_models/02_post_spec.rb
@@ -1,23 +1,23 @@
 describe Post do
   context 'Post validations' do
     it "post is valid" do
-      expect(Post.create(name: "i", content: "hllo")).to be_valid
+      expect(Post.create(name: "hi", content: "hello")).to be_valid
     end
 
     it "is invalid with no name" do
-      expect(Post.create(name: nil, content: "i hve no name")).to_not be_valid
+      expect(Post.create(name: nil, content: "i have no name")).to_not be_valid
     end
 
     it "is invalid with no content" do
-      expect(Post.create(name: "Nme", content: nil)).to_not be_valid
+      expect(Post.create(name: "Name", content: nil)).to_not be_valid
     end
   end
 
   context 'Post relationship to User and Tag' do
-    let(:user) { User.create(name: 'Croshanks') }
-    let(:tag1) { Tag.create(name: 'adorble') }
-    let(:tag2) { Tag.create(name: 'felne') }
-    let(:post) { Post.create(user_id: user.id, name: 'pst', content: 'cotent') }
+    let(:user) { User.create(name: 'Crookshanks') }
+    let(:tag1) { Tag.create(name: 'adorable') }
+    let(:tag2) { Tag.create(name: 'feline') }
+    let(:post) { Post.create(user_id: user.id, name: 'post', content: 'content') }
 
     before do
       PostTag.create(tag_id: tag1.id, post_id: post.id)
