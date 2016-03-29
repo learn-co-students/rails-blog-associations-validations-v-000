0812746ca0fa2085b98f4ab5901e521f1e56d244
authorbeard <msnolan@gmail.com>
2016-03-29 17:10:18 -0400

    all validations passing

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,4 +4,6 @@ class Post < ActiveRecord::Base
   has_many :post_tags
   has_many :tags, through: :post_tags
 
+  validates_presence_of :name, :content
+
 end
--- app/models/post_tag.rb
+++ app/models/post_tag.rb
@@ -2,4 +2,6 @@ class PostTag < ActiveRecord::Base
 
   belongs_to :post
   belongs_to :tag
+
+  
 end
