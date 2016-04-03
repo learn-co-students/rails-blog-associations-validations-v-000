5a1e20021739012cee16de016880578094585bce
Tom Johns <tommyskies@hotmail.com>
2016-04-03 16:21:10 -0600

    Done.

--- db/migrate/20140310213347_create_posts.rb
+++ db/migrate/20140310213347_create_posts.rb
@@ -2,7 +2,8 @@ class CreatePosts < ActiveRecord::Migration
   def change
     create_table :posts do |t|
       t.string :name
-
+      t.belongs_to :user
+      t.text :content
       t.timestamps null: false
     end
   end
