2a4d713080f96819810e694abe7e605c3d5849cd
hudbuz <hzb2@cornell.edu>
2016-05-10 00:45:58 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 RailsBlog::Application.routes.draw do
 
+  resources :tags, only: [:index, :new, :create, :show, :edit, :update, :destroy]
+
+  resources :users, only: [:index, :new, :create, :show, :edit, :update, :destroy]
+
+  resources :posts, only: [:index, :new, :create, :show, :edit, :update, :destroy]
+
 end
