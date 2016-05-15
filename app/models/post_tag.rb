f70e0b7d428ee67f1ff3099eb076f86ce78fc964
Joshua Azemoh <joshazemoh@gmail.com>
2016-05-15 22:19:50 +0100

    Build post-tag association

---
+++ app/models/post_tag.rb
@@ -0,0 +1,5 @@
+class PostTag < ActiveRecord::Base
+  # Associations
+  belongs_to :post
+  belongs_to :tag
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160515205907) do
+ActiveRecord::Schema.define(version: 20160515210758) do
+
+  create_table "post_tags", force: :cascade do |t|
+    t.integer  "tag_id"
+    t.integer  "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string   "name"
@@ -19,6 +26,12 @@ ActiveRecord::Schema.define(version: 20160515205907) do
     t.datetime "updated_at", null: false
     t.text     "content"
     t.integer  "user_id"
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
