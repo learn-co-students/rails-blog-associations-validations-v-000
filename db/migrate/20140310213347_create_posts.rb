a182feb05b7ee1090122198f67fae39ccbe14d43
breiss <brad.p.reiss@gmail.com>
2016-06-18 07:48:59 +0000

    Done.

--- db/migrate/20140310213347_create_posts.rb
+++ db/migrate/20140310213347_create_posts.rb
@@ -2,8 +2,11 @@ class CreatePosts < ActiveRecord::Migration
   def change
     create_table :posts do |t|
       t.string :name
+      t.text :content
+      t.references :user
 
       t.timestamps null: false
     end
   end
 end
+
