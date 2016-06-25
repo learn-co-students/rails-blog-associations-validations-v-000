bbb53356c5171db8a5b0d0f240a0ced132c5043a
interestinall <justin.lefurjah@gmail.com>
2016-06-25 15:31:11 -0500

    make model relationships pass tests

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,5 +3,9 @@ class Post < ActiveRecord::Base
   belongs_to :user
   has_many :post_tags
   has_many :tags, through: :post_tags
-  
+
+
+  validates :content, presence: true
+  validates :name, presence: true
+
 end
