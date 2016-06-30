becd14681eaafb61ff150aca7089a3e59fa638c2
hangloos <ericloos00@gmail.com>
2016-06-30 12:42:40 -0400

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -68,6 +68,6 @@ class PostsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def post_params
-      params.require(:post).permit(:name, :content, :user_id, :tags => [])
+      params.require(:post).permit(:name, :content, :user_id, :tag_ids => [])
     end
 end
