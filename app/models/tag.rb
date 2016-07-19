fcaf04f0744ceb9184bdd03637ab1d21da13f5a9
Christian Whitesides <whitesidescr@gmail.com>
2016-07-19 12:44:57 -0600

    Done.

---
+++ app/models/tag.rb
@@ -0,0 +1,10 @@
+class Tag < ActiveRecord::Base
+  has_many :post_tags
+  has_many :posts, through: :post_tags
+
+  validates :name, uniqueness: true
+
+  def tag_name
+    "#{self.name}"
+  end
+end
