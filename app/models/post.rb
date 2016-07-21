cc9d51d8a4d8cc0a83513ca69df9f3148f7f28a8
jbok4 <jbok4@aol.com>
2016-07-21 16:23:26 -0400

    users views re-added

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,8 @@ class Post < ActiveRecord::Base
   belongs_to :user
   has_many :post_tags
   has_many :tags, through: :post_tags
+    accepts_nested_attributes_for :tags, reject_if: proc { |attributes| attributes['name'].blank? }
+
 
   validates :name, presence: true
   validates :content, presence: true
