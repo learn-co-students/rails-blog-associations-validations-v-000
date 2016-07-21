a49238f98906e7d50c3e5bfeeee2beecfbb9d8c2
jbok4 <jbok4@aol.com>
2016-07-21 16:34:22 -0400

    user_id migration added to posts

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160721191715) do
+ActiveRecord::Schema.define(version: 20160721203312) do
 
   create_table "post_tags", force: :cascade do |t|
     t.integer "post_id"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20160721191715) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text     "content"
+    t.integer  "user_id"
   end
 
   create_table "tags", force: :cascade do |t|
