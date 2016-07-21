e7f87370777cc2299af30a19dea5df97c6e3fcfd
jbok4 <jbok4@aol.com>
2016-07-21 15:27:56 -0400

    post_tags and some views forms

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160720181727) do
+ActiveRecord::Schema.define(version: 20160721191715) do
+
+  create_table "post_tags", force: :cascade do |t|
+    t.integer "post_id"
+    t.integer "tag_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string   "name"
