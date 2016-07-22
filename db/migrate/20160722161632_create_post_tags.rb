36176f12983c281cb8098feec8631cfdfeb9c754
ldgarber <leah.garber88@gmail.com>
2016-07-22 14:50:38 -0400

    Done.

--- db/migrate/20160722161632_create_post_tags.rb
+++ db/migrate/20160722161632_create_post_tags.rb
@@ -1,10 +1,8 @@
 class CreatePostTags < ActiveRecord::Migration
   def change
     create_table :post_tags do |t|
-      t.integer :tag_id
       t.integer :post_id
-
-      t.timestamps null: false
+      t.integer :tag_id
     end
   end
 end
