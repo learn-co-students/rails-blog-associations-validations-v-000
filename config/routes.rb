6e53f9786a4cd4b272edf59edf5d391caedc7074
aprietof <adrianprieto00@gmail.com>
2016-07-26 22:12:00 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 RailsBlog::Application.routes.draw do
 
+  resources :post_tags, only:  [:index, :new, :show, :create, :edit, :update, :destroy]
+  resources :posts, only:  [:index, :new, :show, :create, :edit, :update, :destroy]
+  resources :users, only:  [:index, :new, :show, :create, :edit, :update, :destroy]
+  resources :tags, only:  [:index, :new, :show, :create, :edit, :update, :destroy]
+
 end
---
+++ db/migrate/20160727010617_rename_column_post_tags.rb
@@ -0,0 +1,5 @@
+class RenameColumnPostTags < ActiveRecord::Migration
+  def change
+    rename_column :post_tags, :user_id, :tag_id
+  end
+end
