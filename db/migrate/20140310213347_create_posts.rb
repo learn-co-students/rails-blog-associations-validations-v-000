5928ad85f4816c2560540a1b85c1ac82634a62fb
miclp <michaelpeimani@gmail.com>
2016-08-24 21:28:48 -0700

    Add all associations and models

--- db/migrate/20140310213347_create_posts.rb
+++ db/migrate/20140310213347_create_posts.rb
@@ -1,6 +1,8 @@
 class CreatePosts < ActiveRecord::Migration
   def change
     create_table :posts do |t|
+      t.belongs_to :user, index: true
+      
       t.string :name
       t.text :content
       t.timestamps null: false
