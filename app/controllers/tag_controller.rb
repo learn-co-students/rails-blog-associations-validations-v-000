8a0bdb82686328279c98324faf7a536c03beba79
Traci7822 <Tracir@gmail.com>
2016-09-30 14:31:57 -0600

    Done.

--- app/controllers/tag_controller.rb
+++ app/controllers/tag_controller.rb
@@ -1,2 +1,3 @@
 class TagController < ApplicationController
+
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,10 @@
 class Post < ActiveRecord::Base
   validates :name, presence: true
   validates :name, uniqueness: true
-  validates :content, presence: true
+  validates :content, presence: true, allow_blank: false
 
   belongs_to :user
   has_many :post_tags
   has_many :tags, through: :post_tags
+
 end
