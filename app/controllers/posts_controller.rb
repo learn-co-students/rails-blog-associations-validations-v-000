7772b2310257f7d0b64444270d9efb4eb6297032
Clare <clare.e.horton@gmail.com>
2016-11-07 15:54:52 +0000

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -25,6 +25,9 @@ class PostsController < ApplicationController
   # POST /posts.json
   def create
     @post = Post.new(post_params)
+    tag_params[:tag_ids].each do |id|
+      @post.post_tags.build(tag_id: id)
+    end
 
     respond_to do |format|
       if @post.save
@@ -69,6 +72,10 @@ class PostsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def post_params
-      params.require(:post).permit(:name)
+      params.require(:post).permit(:name, :content)
+    end
+    
+    def tag_params
+      params.require(:post).permit(:tag_ids => [])
     end
 end
