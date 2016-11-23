dd1878ed3a703ae5a6e8dadf736379609a4f28e6
Motti <=>
2016-11-23 13:51:31 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,9 @@
 class Post < ActiveRecord::Base
 	validates :name,:content, presence: true
 
+	 belongs_to :user 
+	 has_many :post_tags
 
 	 has_many :tags, :through => :post_tags
- 	 belongs_to :user
- 	 has_many :tags
+	 
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,5 +1,5 @@
-class Tag < ActiveRecord::Base
-	belongs_to :post
-	  has_many :posts, :through => :post_tags
-
+class Tag < ActiveRecord::Base 
+	  has_many :post_tags
+	  has_many :posts,  :through => :post_tags
+	  validates :name, uniqueness: true
 end
