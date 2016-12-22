f262c2b8faa0ad944eac50729b4d27c608e38a10
Baruch Steinmetz <boruchsteinmetz@outlook.com>
2016-12-22 23:30:24 +0000

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161222151250) do
+ActiveRecord::Schema.define(version: 20161222231735) do
+
+  create_table "post_tags", force: :cascade do |t|
+    t.integer "post_id"
+    t.integer "tag_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string   "name"
     t.text     "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
 
   create_table "tags", force: :cascade do |t|
