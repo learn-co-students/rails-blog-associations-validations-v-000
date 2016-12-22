2f27446a9b0e43f5026a9c73eed614ce240c53a4
Baruch Steinmetz <boruchsteinmetz@outlook.com>
2016-12-22 21:19:23 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,5 @@
 class Post < ActiveRecord::Base
-  
+  belongs_to :user
+  validates :name, :content, presence: true
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ActiveRecord::Base
+  has_many :posts
+  validates :name, uniqueness: true, presence: true
 end
