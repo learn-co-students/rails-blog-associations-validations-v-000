af37f02a84249862603042c8d04ff995e3e16d78
Scott Savage <snsavage@gmail.com>
2017-01-12 16:06:24 -0500

    Add validations and relations to User

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170112204259) do
+ActiveRecord::Schema.define(version: 20170112210510) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
+    t.text     "content"
   end
 
   create_table "tags", force: :cascade do |t|
