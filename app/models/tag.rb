25663e3aa116337a1e2ecad404cb11a4f4c99a27
Michael Burak <michaelrburak@gmail.com>
2017-04-11 11:08:07 -0400

    Done.

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,5 +1,5 @@
 class Tag < ActiveRecord::Base
-  validates :name, uniqueness:true
+  validates :name, uniqueness: true
   belongs_to :user
   has_many :post_tags
   has_many :posts, through: :post_tags
