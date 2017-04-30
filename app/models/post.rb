78f7cedb23d2c4752f073e47077cbc608e631386
anytimesoon <damien@delartigue.co.uk>
2017-04-30 16:02:42 +0100

    Post model is working

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,5 @@
 class Post < ActiveRecord::Base
-  
+  validates :name, :content, presence: true
+  belongs_to :user
+  has_many :tags
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170430145021) do
+ActiveRecord::Schema.define(version: 20170430145957) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text     "content"
   end
 
   create_table "tags", force: :cascade do |t|
