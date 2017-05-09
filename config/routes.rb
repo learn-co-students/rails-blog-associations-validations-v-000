7d2374141abfcfc8e297325aa097198877fdc30c
Annette  Drapalski <annette.drapalski@gmail.com>
2017-05-09 03:36:09 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,29 @@
 RailsBlog::Application.routes.draw do
 
+  get 'tags/new'
+
+  get 'tags/index'
+
+  get 'tags/create'
+
+  get 'tags/edit'
+
+  get 'tags/update'
+
+  get 'tags/destroy'
+
+  get 'users/new'
+
+  get 'users/index'
+
+  get 'users/create'
+
+  get 'users/edit'
+
+  get 'users/update'
+
+  get 'users/destroy'
+
+  resources :posts
+
 end
