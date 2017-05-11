6f24d6d492cefda133b6af60e5a73d6b4e170a3e
mjdele <mike.delehant@gmail.com>
2017-05-11 13:41:57 -0400

    Done.

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,5 +1,6 @@
 class Tag < ActiveRecord::Base
   validates :name, uniqueness: true
+#  validates_uniqueness_of :name
 
   has_many :post_tags
   has_many :posts, through: :post_tags
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
   validates :name, presence: true, uniqueness: true
+#  validates_uniqueness_of :name
+#  validates_presence_of :name
   has_many :posts
 end
