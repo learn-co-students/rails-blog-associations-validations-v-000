633d40a1c63701cf14fdbb192c281e3d266febea
Shmully Wolfson <shmullywolfson@gmail.com>
2017-06-08 11:27:52 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 RailsBlog::Application.routes.draw do
-
+  resources :tags
+  resources :posts
+  resources :users
+  resources :post_tags
+  resources :tags
+  resources :users
+  resources :posts
 end
