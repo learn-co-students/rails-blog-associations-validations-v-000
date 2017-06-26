b852768202554e4932cb4b8845d5fd812c234d68
Aditi Green <asaki444@gmail.com>
2017-06-26 00:37:51 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ActiveRecord::Base
    belongs_to :user
-   has_one :postTag
-   has_many :tags, through: :postTag
+   has_many :postTags
+   has_many :tags, through: :postTags
    validates :name, presence: true, uniqueness: true
    validates :content, presence: true
 end
