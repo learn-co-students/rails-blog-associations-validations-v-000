0c28714b0c97b78d012b3fcc811951cbe3fafe54
WilliamBarela <william.barela.dev@gmail.com>
2017-07-04 18:11:39 +0900

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170704083922) do
+ActiveRecord::Schema.define(version: 20170704090044) do
+
+  create_table "post_tags", force: :cascade do |t|
+    t.integer  "tag_id"
+    t.integer  "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string   "name"
@@ -25,6 +32,7 @@ ActiveRecord::Schema.define(version: 20170704083922) do
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "post_id"
   end
 
   create_table "users", force: :cascade do |t|
