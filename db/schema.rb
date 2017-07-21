a443943209e3e6111d307d2d6f286ec3e8b04c02
Luisa Scavo <luisascavo@gmail.com>
2017-07-21 02:08:09 +0000

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20170710224405) do
   create_table "posts", force: :cascade do |t|
     t.string   "name"
     t.text     "content"
+    t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
