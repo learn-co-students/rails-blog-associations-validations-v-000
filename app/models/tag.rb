fe4c60fb89575217481fee178bc2eaa01caf67ed
Susanna Klosko <sdklos@gmail.com>
2017-08-01 03:40:01 +0000

    Done.

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -2,4 +2,6 @@ class Tag < ActiveRecord::Base
   has_many :post_tags
   has_many :posts, through: :post_tags
   has_many :users, through: :posts
+
+  validates :name, uniqueness: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,7 @@ class User < ActiveRecord::Base
   has_many :posts
   has_many :post_tags, through: :posts
   has_many :tags, through: :post_tags
+
+  validates :name, uniqueness: true
+  validates :name, presence: true
 end
