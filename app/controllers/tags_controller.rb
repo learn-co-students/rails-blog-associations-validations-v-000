264304ab4e9568ec1db9b902940657a7b5231b4b
Spencer Evans <spencevans@gmail.com>
2017-08-04 18:16:26 +0000

    Done.

---
+++ app/controllers/tags_controller.rb
@@ -0,0 +1,37 @@
+class TagsController < ApplicationController
+  before_action :set_tag, only: [:show, :edit, :update]
+
+  def index
+    @tags = Tag.all
+  end
+
+  def new
+    @tag = Tag.new
+  end
+
+  def create
+    @tag = Tag.new(tag_params)
+    if @tag.save
+      redirect_to tag_path(@tag)
+    else
+      render :new
+  end
+
+  def show
+    @tag = Tag.find(params[:id])
+  end
+
+  def edit
+
+  end
+
+  private
+
+    def set_tag
+      @tag = Tag.find(params[:id])
+    end
+
+    def tag_params
+      params.require(:tag).permit(:name)
+    end
+end
