a243a8c15c38b2258cd2120a806e03a0138b71fb
Terry Blue <terryblue@comcast.net>
2017-08-24 22:57:26 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,5 @@
 class Post < ActiveRecord::Base
-  
+  belongs_to :user
+  has_and_belongs_to_many :tags
+  validates_presence_of :name, :content
 end
---
+++ app/models/user.rb
@@ -0,0 +1,4 @@
+class User < ActiveRecord::Base
+  has_many :posts
+  validates_uniqueness_of :name
+end
