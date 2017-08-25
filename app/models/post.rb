45b9826c272b97d7f501660d4d3da10610c5737f
Terry Blue <terryblue@comcast.net>
2017-08-25 17:49:04 +0000

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ActiveRecord::Base
   belongs_to :user
-  has_and_belongs_to_many :tags
+  has_many :posts_tags
+  has_many :tags, through: :posts_tags
   validates_presence_of :name, :content
 end
--- app/models/posts_tag.rb
+++ app/models/posts_tag.rb
@@ -1,3 +1,4 @@
 class PostsTag < ActiveRecord::Base
-
+  belongs_to :post
+  belongs_to :tag
 end
