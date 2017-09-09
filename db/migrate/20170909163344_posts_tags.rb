37d092b4f15f3af29f2a5b058e9398cf66ff6ea0
James Hotchkiss <jimbohotchkiss@yahoo.com>
2017-09-09 18:43:12 +0000

    Done.

--- db/migrate/20170909163344_posts_tags.rb
+++ db/migrate/20170909163344_posts_tags.rb
@@ -1,8 +1,8 @@
 class PostsTags < ActiveRecord::Migration
   def change
-    create_table :poststags do |t|
-      t.integer :tag_id 
+    create_table :post_tags do |t|
+      t.integer :tag_id
       t.integer :post_id 
-    end 
+    end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170909170752) do
+ActiveRecord::Schema.define(version: 20170909163344) do
+
+  create_table "post_tags", force: :cascade do |t|
+    t.integer "tag_id"
+    t.integer "post_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string   "name"
@@ -19,11 +24,6 @@ ActiveRecord::Schema.define(version: 20170909170752) do
     t.datetime "updated_at", null: false
     t.text     "content"
     t.integer  "user_id"
-  end
-
-  create_table "posttags", force: :cascade do |t|
-    t.integer "tag_id"
-    t.integer "post_id"
   end
 
   create_table "tags", force: :cascade do |t|
