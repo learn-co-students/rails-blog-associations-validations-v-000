cd3ac0c34a8db2a303629ab563b5f00a0b5be071
leog7one <lguzman@me.com>
2017-09-18 22:29:38 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,10 @@
 class Post < ActiveRecord::Base
   
+  belongs_to :user
+  has_many :post_tags
+  has_many :tags, :through => :post_tags
+
+#Validates that the specified attributes are not blank
+  validates_presence_of :name, :content
+
 end
