0cd893527444e7a9d6e9b9fd14d73913a036ae4b
charlie lee <ctlee9096@gmail.com>
2017-10-01 20:29:33 +0000

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -20,9 +20,9 @@ ActiveRecord::Schema.define(version: 20171001194445) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "name"
+    t.text     "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.text     "content"
     t.integer  "user_id"
   end
 
