5e6d52364354e51839a563d9f38d144e9ca7a5a9
Nicholas D'Amico <nickalan82@icloud.com>
2017-10-05 21:48:38 -0400

    added content column to Posts db table, All spec for 01-user_spec passing.

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171006014313) do
+ActiveRecord::Schema.define(version: 20171006014704) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "user_id"
+    t.text     "content"
   end
 
   add_index "posts", ["user_id"], name: "index_posts_on_user_id"
