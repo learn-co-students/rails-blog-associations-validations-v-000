4d5add88164199246e2f20dc5ba23e87b2d13a86
Nicholas D'Amico <nickalan82@icloud.com>
2017-10-05 22:00:46 -0400

    Added model Tags with migration,model, controller, view files and directories

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171006014704) do
+ActiveRecord::Schema.define(version: 20171006015907) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "name"
@@ -22,6 +22,12 @@ ActiveRecord::Schema.define(version: 20171006014704) do
   end
 
   add_index "posts", ["user_id"], name: "index_posts_on_user_id"
+
+  create_table "tags", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
