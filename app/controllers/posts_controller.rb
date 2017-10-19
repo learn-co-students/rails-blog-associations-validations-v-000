a329f960b103a2dfc5972d9bf02f93f6761426fe
Jim Stricker <jmstricker93@gmail.com>
2017-10-19 13:03:43 -0400

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -69,6 +69,7 @@ class PostsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def post_params
-      params.require(:post).permit(:name)
+      binding.pry
+      params.require(:post).permit(:name, :user_name, :content, :tag_ids => [])
     end
 end
