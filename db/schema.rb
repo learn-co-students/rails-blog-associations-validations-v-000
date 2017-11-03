cad652dd12e2d9e10c29562ad2f01587be01293d
lpassamano <leighayna.josele@gmail.com>
2017-11-03 18:13:19 -0400

    updated posts table to include user_id

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171103220835) do
+ActiveRecord::Schema.define(version: 20171103221132) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text     "content"
+    t.integer  "user_id"
   end
 
   create_table "tags", force: :cascade do |t|
