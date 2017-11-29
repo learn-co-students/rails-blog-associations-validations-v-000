be42e8e304f318f942eeee941d28691e26564a7c
chrisjamesr <chrisjames.r@gmail.com>
2017-11-28 23:17:21 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,36 @@
 RailsBlog::Application.routes.draw do
+  get 'users/new'
+
+  get 'users/create'
+
+  get 'users/edit'
+
+  get 'users/update'
+
+  get 'users/destroy'
+
+  get 'users/index'
+
+  get 'users/show'
+
+  get 'tags/new'
+
+  get 'tags/create'
+
+  get 'tags/edit'
+
+  get 'tags/update'
+
+  get 'tags/destroy'
+
+  get 'tags/index'
+
+  get 'tags/show'
+
+  resources :posts
+
+  resources :tags
+
+  resources :users
 
 end
