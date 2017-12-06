9f35b5bd87e2bbd55be947ecea75c4fe1f70d3ab
Ben <baschenbrenner@hotmail.com>
2017-12-06 19:34:43 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 RailsBlog::Application.routes.draw do
 
+  resources :post_tags
+
+  resources :tags
+
+  resources :users
+  
+  resources :posts, only: [:new, :edit, :show, :create, :update, :edit]
+
 end
