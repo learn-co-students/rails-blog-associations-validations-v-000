581b8408f87bd569dfbd98b183050455a6ddcfc2
Sam Marshall <samuel.r.marshall@gmail.com>
2017-12-13 23:19:04 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 RailsBlog::Application.routes.draw do
 
+  resources :posts
+
   resources :tags
 
   resources :users
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171214020419) do
+ActiveRecord::Schema.define(version: 20171214023753) do
 
   create_table "post_tags", force: :cascade do |t|
     t.integer "post_id"
@@ -23,16 +23,19 @@ ActiveRecord::Schema.define(version: 20171214020419) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text     "content"
+    t.integer  "user_id"
   end
 
   create_table "tags", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "name"
   end
 
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "name"
   end
 
 end
