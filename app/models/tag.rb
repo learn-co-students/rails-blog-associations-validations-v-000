9899dbca54c498351eeda28901349ea5b0660dc7
Brittany Hartmire <bkhartmire@gmail.com>
2017-12-28 03:45:27 +0000

    Done.

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -2,5 +2,5 @@ class Tag < ActiveRecord::Base
   validates :name, uniqueness: true
 
   has_many :post_tags
-  has_many :tags, through: :post_tags
+  has_many :posts, through: :post_tags
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
   validates :name, uniqueness: true
-
+  validates :name, presence: true
+  
   has_many :posts
 end
