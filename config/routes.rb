0d2fd91386274fe294d97ca6cef4545c1cbcd2ea
Michael Mirzakandov <mikemirzak@aol.com>
2018-01-18 17:52:01 +0000

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 RailsBlog::Application.routes.draw do
+    resources :posts, only: [:index, :new, :create, :show, :edit, :update, :destroy]
 
+   resources :tags, only: [:index, :new, :create, :show, :edit, :update, :destroy]
+
+   resources :users, only: [:index, :new, :create, :show, :edit, :update, :destroy]
 end
