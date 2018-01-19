a09e86af0b4f83e992fff22ed82338dac933cb1b
daniellecroxton <danikrist@gmail.com>
2018-01-18 23:14:48 -0700

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,6 @@ class Post < ActiveRecord::Base
   validates :name, presence: true
   validates :content, presence: true
   belongs_to :user
-  has_many :tags
+  has_many :posts_tags
+  has_many :tags, through: :posts_tags
 end
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,4 +1,5 @@
 class Tag < ActiveRecord::Base
   validates :name, uniqueness: true
-  has_many :posts
+  has_many :posts_tags
+  has_many :posts, through: :posts_tags
 end
