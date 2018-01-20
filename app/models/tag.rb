3749b73d4a960b25503e615f285115c36a61d20a
daniellecroxton <danikrist@gmail.com>
2018-01-19 23:20:55 -0700

    Done.

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,5 +1,5 @@
 class Tag < ActiveRecord::Base
   validates :name, uniqueness: true
-  has_many :posts_tags
-  has_many :posts, through: :posts_tags
+  has_many :post_tags
+  has_many :posts, through: :post_tags
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,18 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180119061132) do
+ActiveRecord::Schema.define(version: 20180120053652) do
+
+  create_table "post_tags", id: false, force: :cascade do |t|
+    t.integer "post_id", null: false
+    t.integer "tag_id",  null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text     "content"
-  end
-
-  create_table "posts_tags", id: false, force: :cascade do |t|
-    t.integer "post_id", null: false
-    t.integer "tag_id",  null: false
+    t.integer  "user_id"
   end
 
   create_table "tags", force: :cascade do |t|
