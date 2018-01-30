acb58051d0150c11fdbad285da9cf23c0dc713ab
Richard Steinbach <steinbach.dick@gmail.com>
2018-01-30 12:47:12 -0800

    rails-blog-associations-validations-v-000

---
+++ app/controllers/tags_controller.rb
@@ -0,0 +1,14 @@
+class TagsController < ApplicationController
+  def edit
+  end
+
+  def index
+    @tags = Tag.all
+  end
+
+  def new
+  end
+
+  def show
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 RailsBlog::Application.routes.draw do
 
+  resources :tags, only: [:index, :show, :new, :create, :edit, :update]
+  resources :posts, only: [:index, :show, :new, :create, :edit, :update]
+  resources :users, only: [:index, :show, :new, :create, :edit, :update]
 end
