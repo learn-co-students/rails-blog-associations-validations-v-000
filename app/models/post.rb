17359fa3b08245e583186f3d2ff1e74960d2753c
rh24 <ms.rebeccahong@gmail.com>
2018-03-05 17:09:28 -0500

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
-  validates_presence_of :name, :content
+  validates :name, :content, { presence: true }
   belongs_to :user
   has_many :post_tags
   has_many :tags, through: :post_tags
