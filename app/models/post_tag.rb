58d82a74db73d4236484b062a0a9bc33901bd019
NolanHughes <Nhughes987@gmail.com>
2018-03-08 00:18:06 -0600

    Done.

---
+++ app/models/post_tag.rb
@@ -0,0 +1,5 @@
+class PostTag < ActiveRecord::Base
+  belongs_to :post
+  belongs_to :tag
+end
+#***STUDY THIS SHIT!!!! IT'S DUMB AS HELL!!!***
---
+++ db/migrate/20180307203439_post_tag.rb
@@ -0,0 +1,8 @@
+class PostTag < ActiveRecord::Migration
+  def change
+    create_table :post_tag, :id => false do |t|
+      t.integer :post_id, null: false
+      t.integer :tag_id, null: false
+    end
+  end
+end
