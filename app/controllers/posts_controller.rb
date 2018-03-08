925080148e264e34ffaa1fc79a97b2c0edc722d5
catalanowebdevelopment <catalano.work@gmail.com>
2018-03-08 11:22:05 -0500

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,74 +1,30 @@
 class PostsController < ApplicationController
-  before_action :set_post, only: [:show, :edit, :update, :destroy]
-
-  # GET /posts
-  # GET /posts.json
-  def index
-    @posts = Post.all
-  end
-
-  # GET /posts/1
-  # GET /posts/1.json
-  def show
-  end
-
-  # GET /posts/new
-  def new
-    @post = Post.new
-  end
-
-  # GET /posts/1/edit
-  def edit
-  end
-
-  # POST /posts
-  # POST /posts.json
-  def create
-    @post = Post.new(post_params)
-
-    respond_to do |format|
-      if @post.save
-        format.html { redirect_to @post, notice: 'Post was successfully created.' }
-        format.json { render action: 'show', status: :created, location: @post }
-      else
-        format.html { render action: 'new' }
-        format.json { render json: @post.errors, status: :unprocessable_entity }
-      end
+    def new
+        @post = Post.new
     end
-  end
-
-  # PATCH/PUT /posts/1
-  # PATCH/PUT /posts/1.json
-  def update
-    respond_to do |format|
-      if @post.update(post_params)
-        format.html { redirect_to @post, notice: 'Post was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: 'edit' }
-        format.json { render json: @post.errors, status: :unprocessable_entity }
-      end
+    
+    def create 
+        @post = Post.create(post_params(:name, :content, :tag_ids))
+        
+        if @post.save
+            redirect_to post_path(@post)
+        else
+            render :new
+        end
     end
-  end
-
-  # DELETE /posts/1
-  # DELETE /posts/1.json
-  def destroy
-    @post.destroy
-    respond_to do |format|
-      format.html { redirect_to posts_url }
-      format.json { head :no_content }
+    
+    def show
+        set_post
     end
-  end
-
-  private
-    # Use callbacks to share common setup or constraints between actions.
+    
+    
+    private
+    
+    def post_params(*args)
+        params.require(:post).permit(:name, :content, :tag_ids => [])
+    end
+    
     def set_post
-      @post = Post.find(params[:id])
-    end
-
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def post_params
-      params.require(:post).permit(:name)
+        @post = Post.find(params[:id])
     end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
-class Post < ActiveRecord::Base
-    belongs_to :user
+class User < ActiveRecord::Base
     has_many :post_tags
     has_many :tags, through: :post_tags
+    has_many :posts
     
     validates :name, presence: true
-    validates :content, presence: true
+    validates :name, uniqueness: true
 end
