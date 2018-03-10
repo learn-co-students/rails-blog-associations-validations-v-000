47d02dbbeead9cfc592a92187057a44faf3e0dde
An Nuyen <annuyen@gmail.com>
2018-03-10 19:06:26 +0000

    Done.

---
+++ db/migrate/20180310190024_create_post_tags.rb
@@ -0,0 +1,11 @@
+class CreatePostTags < ActiveRecord::Migration
+  def change
+    create_table :post_tags do |t|
+      t.string :name
+      t.integer :post_id
+      t.integer :tag_id
+
+      t.timestamps null: false
+    end
+  end
+end
