91cf788c549db6fdbae60b74a23ced81c0cdbefa
funemployeddad <funemployeddad@gmail.com>
2018-03-12 00:32:55 -0700

    working on associations for PostTag. stuck on Post has_many :tags

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180312072113) do
+ActiveRecord::Schema.define(version: 20180312072924) do
 
   create_table "post_tags", force: :cascade do |t|
     t.integer  "post_id"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 20180312072113) do
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "post_id"
   end
 
   create_table "users", force: :cascade do |t|
