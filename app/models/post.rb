8febc6a54f0c119978bebc93193d92b5fef88dec
funemployeddad <funemployeddad@gmail.com>
2018-03-11 22:47:18 -0700

    User all done. Post partially done, working through join table

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,8 @@
 class Post < ActiveRecord::Base
-  
+
+  belongs_to :user
+
+  validates :name, presence: true
+  validates :content, presence: true
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ActiveRecord::Base
+
+  has_many :posts
+  
+  validates :name, presence: true
+  validates :name, uniqueness: true
+
+
 end
