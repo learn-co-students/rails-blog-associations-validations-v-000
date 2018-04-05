125ec3d245f650519673b1afc3082bcb15241cb1
Andrew Joshowitz <ajoshow@sas.upenn.edu>
2018-04-05 15:56:56 +0000

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -11,8 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 20180405150210) do
+ActiveRecord::Schema.define(version: 20180405155014) do
 
   create_table "post_tags", force: :cascade do |t|
     t.integer  "post_id"
@@ -20,18 +19,15 @@ ActiveRecord::Schema.define(version: 20180405150210) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-=======
-ActiveRecord::Schema.define(version: 20180405145322) do
->>>>>>> d3b5dfc332cd09de8a958b37ebd513e9c65b361d
 
   create_table "posts", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text     "content"
+    t.integer  "user_id"
   end
 
-<<<<<<< HEAD
   create_table "tags", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
@@ -44,6 +40,4 @@ ActiveRecord::Schema.define(version: 20180405145322) do
     t.datetime "updated_at", null: false
   end
 
-=======
->>>>>>> d3b5dfc332cd09de8a958b37ebd513e9c65b361d
 end
