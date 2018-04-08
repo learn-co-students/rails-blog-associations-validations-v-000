6d8d356f8f2f30bc06dc00257ffc1aa4ce7bd056
Penguino <heriberto@melo.nyc>
2018-04-08 00:33:03 -0400

    Done.

--- db/migrate/20140310213347_create_posts.rb
+++ db/migrate/20140310213347_create_posts.rb
@@ -2,6 +2,9 @@ class CreatePosts < ActiveRecord::Migration
   def change
     create_table :posts do |t|
       t.string :name
+      t.text :content
+
+      t.belongs_to :user
 
       t.timestamps null: false
     end
