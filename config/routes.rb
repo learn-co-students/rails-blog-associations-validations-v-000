8186ebe7fee1b20e766d9d1d90696f37cac37310
codeesh <vineshpersaud@gmail.com>
2018-04-10 21:10:24 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,22 @@
 RailsBlog::Application.routes.draw do
 
+  get 'tags/new'
+
+  get 'tags/edit'
+
+  get 'tags/index'
+
+  get 'tags/show'
+
+  get 'user/new'
+
+  get 'user/edit'
+
+  get 'user/index'
+
+  get 'user/show'
+
+  resources :users
+  resources :posts
+  resources :tags
 end
